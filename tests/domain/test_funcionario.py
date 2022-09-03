d4af642dd25bcc8f04821ffa22623db18c1f46d1
Izabel Rodrigues <izabel.castro.rodrigues@gmail.com>
2022-09-03 14:15:45 -0300

    Adicionando teste para cobrir a nova regra para decrescimo de salario
    - Somente podem ter seus salarios reduzidos se tiverem um papel de DIRETOR ou SOCIO e
      tiverem salário maior ou igual a R$100.000,00

--- tests/domain/test_funcionario.py
+++ tests/domain/test_funcionario.py
@@ -9,7 +9,7 @@ class TestFuncionario:
         ano_atual = date.today().year
         idade_esperada = ano_atual - ano_nascimento
 
-        funcionario_mock = Funcionario('Func-Test', '01/03/1980', 10000)
+        funcionario_mock = self.build_mock_funcionario(10000)
 
         # when
         idade_calculada = funcionario_mock.idade()
@@ -50,7 +50,7 @@ class TestFuncionario:
     def test_quando_salario_igual_cem_mil_deve_realizar_decrescimo_dez_porcento(self):
         # given
         salario = 100000
-        funcionario_mock = self.build_mock_funcionario(salario)
+        funcionario_mock = self.build_mock_funcionario(salario, 'DIRETOR')
         novo_salario_esperado = 90000
 
         # when
@@ -63,7 +63,7 @@ class TestFuncionario:
     def test_quando_salario_maior_cem_mil_deve_realizar_decrescimo_dez_porcento(self):
         # given
         salario = 150000
-        funcionario_mock = self.build_mock_funcionario(salario)
+        funcionario_mock = self.build_mock_funcionario(salario,'SOCIO')
         novo_salario_esperado = 135000
 
         # when
@@ -86,5 +86,32 @@ class TestFuncionario:
         # then
         assert novo_salario_calculado == novo_salario_esperado
 
-    def build_mock_funcionario(self, salario):
-        return Funcionario('Func-Test', '01/03/1980', salario)
+    def test_quando_diretor_salario_menor_cem_mil_nao_deve_realizar_decrescimo_dez_porcento(self):
+        # given
+        salario = 90000
+        funcionario_mock = self.build_mock_funcionario(salario, 'DIRETOR')
+        novo_salario_esperado = 90000
+
+        # when
+        funcionario_mock.decrescer_salario()
+        novo_salario_calculado = funcionario_mock._salario
+
+        # then
+        assert novo_salario_calculado == novo_salario_esperado
+
+    def test_quando_socio_salario_menor_cem_mil_nao_deve_realizar_decrescimo_dez_porcento(self):
+        # given
+        salario = 90000
+        funcionario_mock = self.build_mock_funcionario(salario, 'SOCIO')
+        novo_salario_esperado = 90000
+
+        # when
+        funcionario_mock.decrescer_salario()
+        novo_salario_calculado = funcionario_mock._salario
+
+        # then
+        assert novo_salario_calculado == novo_salario_esperado
+
+    def build_mock_funcionario(self, salario, funcao='TEAM'):
+        return Funcionario('Func-Test', '01/03/1980', salario, funcao)
+
